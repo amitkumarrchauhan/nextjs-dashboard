945f229dbcfe62141fce3babb0671088e8ce90ec
Amitkumar Chauhan <chauhan.amitkumar@theoptimum.net>
2025-03-09 19:08:22 +0800

    []: changes done for Chapter 7 - Fetching Data
    2. rendering revenue chart post revenue fetch in async Dashboard Page component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,8 +2,14 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
 
 export default async function DashboardPage() {
+  console.log('app.DashboardPage.render');
+
+  const revenue = await fetchRevenue();
+  console.log('revenue: ', revenue);
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -20,7 +26,7 @@ export default async function DashboardPage() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
+        <RevenueChart revenue={revenue}  />
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>

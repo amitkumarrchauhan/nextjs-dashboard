549ec0c6aa1b8271c5e8136dc3910753337205c7
Amitkumar <chauhan.amitkumar@gmail.com>
2025-03-11 08:32:31 +0800

    []: Chapter 12 - Mutating Data
    Changes done for adding Invoice Create Form

---
+++ .dev-local-setups/git-config.sh
@@ -0,0 +1,11 @@
+git config core.editor code --wait
+
+git config alias.st status
+git config alias.ad add
+git config alias.cm commit
+git config alias.pl pull
+git config alias.plo pull origin
+git config alias.ps push
+git config alias.pso push origin
+git config alias.ui--au update-index --assume-unchanged
+git config alias.ui--nau update-index --no-assume-unchanged

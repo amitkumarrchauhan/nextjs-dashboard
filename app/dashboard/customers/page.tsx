500833bad3e1c267366db5744ed24b8102a7268b
Amitkumar Chauhan <chauhan.amitkumar@theoptimum.net>
2025-03-13 14:51:31 +0800

    []: chapter 16 - Adding Metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,10 @@
-const CustomersPage: React.FC = () => {
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+const CustomersPage = () => {
   console.log('CustomersPage.render');
 
   return (
@@ -6,6 +12,6 @@ const CustomersPage: React.FC = () => {
       <p>Customers Page</p>
     </div>
   );
-}
+};
 
-export default CustomersPage
+export default CustomersPage;

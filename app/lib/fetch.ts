0f9cc440b2b2438a67be8b21866ad7e1162d61c4
Amitkumar <chauhan.amitkumar@gmail.com>
2025-03-21 09:02:04 +0800

    -

--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -3,10 +3,10 @@ const gatewayHost = 'http://localhost:9000';
 const fetchData = async (...args: [RequestInfo, RequestInit?]) => {
   const [url, options] = args;
   const finalUrl = gatewayHost + url;
-  
-  console.log('fetching data from: ', finalUrl);
 
-  return fetch(...[finalUrl, options]);  
+  console.log('\nfetching data from: ', finalUrl);
+
+  return fetch(...[finalUrl, options]);
 };
 
 export { fetchData };
